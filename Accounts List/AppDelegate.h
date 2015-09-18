0778ca10ab5749d61b58d2f005233d59aef22822
Anton Pomozov <pomozov@shtrih-m.ru>
2015-09-18 11:03:41 +0300

    Initial Commit

---
+++ Accounts List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Accounts List
+//
+//  Created by Anton Pomozov on 18.09.15.
+//  Copyright © 2015 Akademon Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

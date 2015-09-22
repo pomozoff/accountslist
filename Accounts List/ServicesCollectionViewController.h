d87910d07c9e37e023c699e7c1ee06a23b4d8ae7
Anton Pomozov <pomozov@shtrih-m.ru>
2015-09-22 13:49:55 +0300

    [Add]: Collection view controler for Services

---
+++ Accounts List/ServicesCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ServicesCollectionViewController.h
+//  Accounts List
+//
+//  Created by Anton Pomozov on 22.09.15.
+//  Copyright © 2015 Akademon Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ServicesCollectionViewController : UICollectionViewController
+
+@end
